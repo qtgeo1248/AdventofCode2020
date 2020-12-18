a5061c149809cd4238dcf973f2553c94b0a91f69
qtgeo1248 <gzhou7819@gmail.com>
2020-12-18 15:30:58 -0500

    Finished Day 2

---
+++ Day2/Day2a.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char *filename = "Day2Passwords.txt";
+    FILE *f = fopen(filename, "r");
+    char *line = NULL;
+    size_t n = 0;
+    size_t valids = 0;
+    while (getline(&line, &n, f) != -1) {
+        char *str = strchr(line, ':');
+        char *pass = calloc(sizeof(char), strlen(str) - 2);
+        strcpy(pass, &str[2]);
+        int low = strtol(line, &line, 10);
+        line++;
+        int high = strtol(line, &line, 10);
+        char ch = line[1];
+        
+        size_t count = 0;
+        for (int i = 0; i < strlen(pass); i++)
+            if (pass[i] == ch) count++;
+        if (low <= count && count <= high)
+            valids++;
+    }
+
+    printf("Answer: %zu\n", valids);
+    fclose(f);
+
+    return 0;
+}
---
+++ Day2/Day2b.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    char *filename = "Day2Passwords.txt";
+    FILE *f = fopen(filename, "r");
+    char *line = NULL;
+    size_t n = 0;
+    size_t valids = 0;
+    while (getline(&line, &n, f) != -1) {
+        char *str = strchr(line, ':');
+        char *pass = calloc(sizeof(char), strlen(str) - 2);
+        strcpy(pass, &str[2]);
+        int i = strtol(line, &line, 10);
+        line++;
+        int j = strtol(line, &line, 10);
+        char c = line[1];
+        
+        if ((pass[i - 1] == c || pass[j - 1] == c) && pass[i - 1] != pass[j - 1])
+            valids++;
+    }
+
+    printf("Answer: %zu\n", valids);
+    fclose(f);
+
+    return 0;
+}
