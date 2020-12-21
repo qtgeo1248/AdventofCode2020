ee8841778d606fb534c14e6191d74c2315ca4aac
qtgeo1248 <gzhou7819@gmail.com>
2020-12-21 03:46:32 -0500

    finished part a of day 5

---
+++ Day5/a.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool is_pass(bool* info) {
+    for (int i = 0; i < 7; i++)
+        if (!info[i]) return false;
+    return true;
+}
+
+int main() {
+    char *filename = "Seats.txt";
+    FILE *f = fopen(filename, "r");
+    char *seat = NULL;
+    size_t n = 0;
+    size_t max = 0;
+
+    while (getline(&seat, &n, f) != -1) {
+        size_t low = 0; size_t high = 127;
+        for (int i = 0; i < 7; i++) {
+            if (seat[i] == 'F') high -= (high - low + 1) / 2;
+            else low += (high - low + 1) / 2;
+        }
+        size_t row = low;
+        low = 0; high = 7;
+        for (int i = 7; i < 10; i++) {
+            if (seat[i] == 'L') high -= (high - low + 1) / 2;
+            else low += (high - low + 1) / 2;
+        }
+        size_t col = low;
+        size_t id = row * 8 + col;
+        if (id > max) max = id;
+    }
+    printf("Answer: %zu\n", max);
+    free(seat);
+    fclose(f);
+    return 0;
+}
---
+++ Day5/b.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main() {
+
+    return 0;
+}
