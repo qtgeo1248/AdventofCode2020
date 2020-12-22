22d5849e25f0370160a7a5f3ad5fd83ed467651b
qtgeo1248 <gzhou7819@gmail.com>
2020-12-22 02:34:28 -0500

    Gave up on Day 7

--- Day7/a.c
+++ Day7/a.c
@@ -3,7 +3,24 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct node_header node;
+struct node_header {
+    char *color;
+    node *prev;
+    node *next;
+};
+
 int main() {
-    
+    char *filename = "Rules.txt";
+    FILE *f = fopen(filename, "r");
+    char *line = NULL; size_t len = 0;
+    size_t count = 0;
+
+    while (getline(&line, &len, f) != -1) {
+        
+    }
+    printf("%zu\n", count);
+    free(line);
+    fclose(f);
     return 0;
 }
